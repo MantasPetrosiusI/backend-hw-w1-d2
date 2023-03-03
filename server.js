a7fb175e28eb0a552fc9f5d31e8d4021f3965e12
MantasPetrosiusI <mantas.petrosius@outlook.com>
2023-03-03 20:32:24 +0000

    fin

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 import express from "express";
+import cors from "cors";
 import endpoints from "express-list-endpoints";
 import authorsRouter from "./authors/index.js";
 
 const server = express();
 
+server.use(cors());
+
 server.use(express.json());
 
 server.use("/authors", authorsRouter);
 
-server.listen(3001, () => {
+server.listen(3025, () => {
   console.table(endpoints(server));
 });
